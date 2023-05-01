0859c7965a95afbf6c1463f5282a0c413963f31e
Laura McGowan <prime.lmcg@gmail.com>
2023-05-01 15:15:49 -0500

    function getString written

--- js/site.js
+++ js/site.js
@@ -1,3 +1,24 @@
-function helloWorld() {
-    alert("Hello World!!");
+
+//Get the string from the page
+//controller function
+function getString() {
+
+    document.getElementById("alert").classList.add("invisible") //classList will bring back all the classes on the alert element
+
+    let userString = document.getElementById("userString").value;
+    let reverseString = reverseString(userString);
+    displayReversedString(reverseString);
+}
+
+//Reverse the string
+//logic function
+//userString is a parameter
+function reverseString(userString) {
+
+}
+
+//Display the reversed string to the user
+//view function
+function displayReversedString() {
+
 }
